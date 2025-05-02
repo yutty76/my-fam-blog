975f72cc1fc3f2a05ee9d23aebf812b9a3d5351e
yutty76 <yakonoba5035@gmail.com>
2025-05-02 17:32:47 +0900

    設定変更

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true, // 必要に応じて他の設定も維持・追加
   images: {
-    domains: ['images.microcms-assets.io'],
+    // ローカル画像の場合、通常ここにドメイン指定は不要
+    // Netlifyプラグインがローダーを自動設定することを期待
+    // 問題が解決しない場合、loader: 'default' などを試すことも検討
   },
+  // 他の既存の設定があればここに追加
 };
 
 module.exports = nextConfig;
